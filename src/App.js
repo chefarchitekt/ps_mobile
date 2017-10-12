166857f337509db7d0ca445aaf520d376657cdc5
chefarchitekt <chefarchitekt@gmail.com>
2017-10-12 14:09:08 +0800

    start for login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Root } from './AppRouter';
 
 
 class App extends Component {
     render() {
         return (
-            <View>
-                <Text>
-                    My First App
-                </Text>
-            </View>
+            <Root />
         );
     }
 }
---
+++ src/screens/Feed.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {
+  ScrollView
+} from 'react-native';
+import { List, ListItem } from 'react-native-elements';
+import { users } from '../locals/mockData';
+
+class Feed extends Component {
+  onLearnMore = (user) => {
+    this.props.navigation.navigate('Details', { ...user });
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <List>
+          {users.map((user) => (
+            <ListItem
+              key={user.login.username}
+              roundAvatar
+              avatar={{ uri: user.picture.thumbnail }}
+              title={`${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()}`}
+              subtitle={user.email}
+              onPress={() => this.onLearnMore(user)}
+            />
+          ))}
+        </List>
+      </ScrollView>
+    );
+  }
+}
+
+export default Feed;
