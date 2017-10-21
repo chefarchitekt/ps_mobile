c1be78c120b7b417ad6753a282942325261c2f6e
chefarchitekt <chefarchitekt@gmail.com>
2017-10-22 06:10:14 +0800

    redefine code architecture

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,7 +1,6 @@
-//import React from 'react';
-//import { Text } from 'react-native';
-import { TabNavigator, StackNavigator } from 'react-navigation';
-import { Icon } from 'react-native-elements';
+//import React from 'react'; import { Text } from 'react-native';
+import {TabNavigator, StackNavigator} from 'react-navigation';
+import {Icon} from 'react-native-elements';
 
 //import screens
 import LoginScreen from './views/screens/Auth/LoginScreen';
@@ -9,21 +8,30 @@ import SignupScreen from './views/screens/Auth/SignupScreen';
 
 export const AuthStack = StackNavigator({
   NavLogin: {
-    screen: LoginScreen,
+    screen: LoginScreen
   },
   NavSignup: {
     screen: SignupScreen
   }
-}, { 
-  headerMode: 'none' 
-});
-  
+}, { headerMode: 'none' });
+
+
+export const RootNavigator =  (isSignedIn) => {
+  return StackNavigator({
+    Authentication: {
+      screen: AuthStack
+    }
+  }, {
+    mode: 'modal',
+    headerMode: 'none'
+  });
+}
+
 export const Root = StackNavigator({
   Authentication: {
     screen: AuthStack
   }
 }, {
   mode: 'modal',
-  headerMode: 'none',
+  headerMode: 'none'
 });
-
